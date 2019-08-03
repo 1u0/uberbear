3ff1d5b373ed7857a44bb82e949c0bc034b670a7
Aleksey Pak <488251+1u0@users.noreply.github.com>
2019-08-03 14:23:06 +0200

    Add unit test for TransferService

---
+++ src/test/kotlin/am/royalbank/uberbear/domain/services/transfers/TransferServiceTest.kt
@@ -0,0 +1,35 @@
+package am.royalbank.uberbear.domain.services.transfers
+
+import am.royalbank.uberbear.domain.entities.EUR
+import am.royalbank.uberbear.frameworks.sql.Db
+import io.mockk.mockk
+import java.util.UUID
+import java.util.concurrent.ThreadLocalRandom
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+internal class TransferServiceTest {
+    private val db = mockk<Db>()
+    private val service = TransferService(db)
+
+    @Test fun `transferring negative amount is not allowed`() {
+        val sourceAccountId = UUID.randomUUID()
+        val targetAccountId = UUID.randomUUID()
+
+        val random = ThreadLocalRandom.current()
+        val amount = random.nextInt(-500_00, 0).EUR
+
+        assertThrows<IllegalArgumentException> {
+            service.makeTransfer(sourceAccountId, targetAccountId, amount, "counter transfer")
+        }
+    }
+
+    @Test fun `transferring zero amount is not allowed`() {
+        val sourceAccountId = UUID.randomUUID()
+        val targetAccountId = UUID.randomUUID()
+
+        assertThrows<IllegalArgumentException> {
+            service.makeTransfer(sourceAccountId, targetAccountId, 0.EUR, "send messages for free")
+        }
+    }
+}
